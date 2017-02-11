abd7fc4885720e1e4981ca77dfaec53b184a8ecc
alec1 <alec1@10.84.111.14>
2017-02-11 18:13:31 -0500

    Starting the building blocks for the JUnit tests

---
+++ src/edu/buffalo/cse116/SetTests.java
@@ -0,0 +1,29 @@
+package edu.buffalo.cse116;
+
+import org.junit.Test;
+
+/** This class provides the JUnit tests that will be used
+ * to make sure that the pixel ---> color translations
+ * are correct for each unique set that's used.
+ * @author Alec Otminski
+ *
+ */
+
+public class SetTests {
+	//Mandelbrot Set Tests
+	@Test
+	public void testMandelbrotEmpty(){
+		
+	}
+	//Julia Set Tests
+	@Test
+	public void testJuliaEmpty(){
+		
+	}
+	//Burning Ship Set Tests
+	@Test
+	public void testBurningShipEmpty(){
+		
+	}
+	
+}
