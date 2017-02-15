0543508f09ba9dccdc483a5c4497f5efe999f37c
stephenfung98 <stephen_fung98@yahoo.com>
2017-02-14 20:20:08 -0500

    added parameters to mandelbrotSet

--- src/edu/buffalo/cse116/MandelbrotSet.java
+++ src/edu/buffalo/cse116/MandelbrotSet.java
@@ -1,11 +1,11 @@
 package edu.buffalo.cse116;
 
-public class MandelbrotSet {
+public class MandelbrotSet{
 	private int xCord = 512;
 	private int yCord = 512;
 	private double xCalc = -2.15 + Math.abs((-2.15 + .6) / 512);
 	private double yCalc = -1.3 + Math.abs((-1.3 + 1.3) / 512);
-	public int[][] mandelbrotSet(){
+	public int[][] mandelbrotSet(double xCalc, double yCalc){
 		for(int i = 0; i < xCord; i++){
 			for(int a = 0; i < yCord; a++){
 				double xPrime = (xCalc * xCalc) - (yCalc * yCalc) + xCalc;
@@ -14,8 +14,7 @@ public class MandelbrotSet {
 				yPrime = yCalc;
 			}
 		}
-		return mandelbrotSet();
+		return mandelbrotSet(yCalc, yCalc);
 		
 	}
 }
-
