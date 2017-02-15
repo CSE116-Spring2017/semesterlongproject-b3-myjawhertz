282663ad63b87d9576ee502b5eb8019464bcfd82
stephenfung98 <stephen_fung98@yahoo.com>
2017-02-14 20:05:50 -0500

    MandelbrotSet (hopefully working??)

--- src/edu/buffalo/cse116/Driver.java
+++ src/edu/buffalo/cse116/Driver.java
@@ -4,6 +4,5 @@ public class Driver {
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-	  this is the change.
   }
 }
--- src/edu/buffalo/cse116/MandelbrotSet.java
+++ src/edu/buffalo/cse116/MandelbrotSet.java
@@ -1,11 +1,20 @@
 package edu.buffalo.cse116;
 
 public class MandelbrotSet {
-
+	private int xCord = 512;
+	private int yCord = 512;
+	private double xCalc = -2.15 + Math.abs((-2.15 + .6) / 512);
+	private double yCalc = -1.3 + Math.abs((-1.3 + 1.3) / 512);
 	public int[][] mandelbrotSet(){
-		int[][] mandelbrotSet = new int[512][512];
-		int xCalc = mandelbrotSet[][-512];
-		return mandelbrotSet;
+		for(int i = 0; i < xCord; i++){
+			for(int a = 0; i < yCord; a++){
+				double xPrime = (xCalc * xCalc) - (yCalc * yCalc) + xCalc;
+				double yPrime = 2 * xCalc * yCalc;
+				xPrime = xCalc;
+				yPrime = yCalc;
+			}
+		}
+		return mandelbrotSet();
 		
 	}
 }
