675cf96773c6a37acdc5a5e6aef315a8d8ffd8b1
ayeshaismail2 <ayeshaotal@gmail.com>
2017-02-17 13:10:53 -0500

    Quick setup

--- src/edu/buffalo/cse116/JuliaSet.java
+++ src/edu/buffalo/cse116/JuliaSet.java
@@ -3,7 +3,17 @@ package edu.buffalo.cse116;
 public class JuliaSet {
 	
 	public JuliaSet(){
+		
+		
 		int [] [] xY = new  int [512][512];
+		int width = xY.length; //row
+		int height = xY[0].length; //column
+		
+		for( int i =0; i<width; i++){
+			for ( int j =0; j<height; j++){
+				// 
+			}
+		}
 		
 	}
 	
