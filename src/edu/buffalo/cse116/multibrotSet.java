9a82041e624cb095ccaccbf48a99b298e2cea891
alec1 <alec1@10.84.122.127>
2017-02-19 18:58:08 -0500

    l

--- src/edu/buffalo/cse116/multibrotSet.java
+++ src/edu/buffalo/cse116/multibrotSet.java
@@ -6,24 +6,11 @@ public class multibrotSet {
 	
 	
 	public int multibrotSet(double xCalc,double yCalc){
-		double x = xCalc;
-		double y = yCalc;
-		
-		while(dist <= 2 && passes < 255){
-			dist = Math.sqrt(Math.pow(xCalc,2) + Math.pow(yCalc,2));	
-			double tempX = xCalc;
-			double tempY = yCalc;
-			
-			xCalc = xCalc + (2/512);
-			yCalc = yCalc + (2.6/512);
-			
-			xCalc = (tempX*tempX*tempX) - (3*tempX*tempY*tempY) + x;
-			yCalc = (3*tempX*tempX*tempY) - (tempY*tempY*tempY) + y;
-			
-			dist = Math.sqrt(Math.pow(xCalc,2) + Math.pow(yCalc,2));
-
-			passes++;
-		}
+		xCalc = xCoordTranslation(xCalc);
 		return passes;
+	}
+	
+	public double xCoordTranslation(double xCalc){
+		xCalc = 
 	}
 }
