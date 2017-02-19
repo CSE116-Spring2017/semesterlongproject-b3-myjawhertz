168e1d1ed0394aef4818b313aea224a4954a6928
alec1 <alec1@10.84.122.127>
2017-02-19 18:27:51 -0500

    tests for Madelbrot class added & deleted an unused class

--- src/edu/buffalo/cse116/SetTests.java
+++ src/edu/buffalo/cse116/SetTests.java
@@ -13,96 +13,79 @@ import org.junit.Test;
 
 public class SetTests {
 	//Mandelbrot Set Tests
-	@Test
-	public void test0() {
-		MandelbrotSet ms = new MandelbrotSet();
-		assertEquals(1, ms.mandelbrotSet(0.5946289062500001, 1.2949218750000122), .0001);
-	}
-	
 	
 	@Test
 	public void testMandelbrotXCoordinateTranslation(){	//this tests is the X-Coordinate Translation works
-		int [][] emptySet = new int[512][512];
 		MandelbrotSet xCoordTest = new MandelbrotSet();
-		assertEquals("Product is -2.15", -2.15, xCoordTest.mandelbrotSet(0, 0));
+		assertEquals("Product is -2.15", -2.15, xCoordTest.mandelbrotSet(0, 0), 0.00001);
 	}
 	@Test
 	public void testMandelbrotYCoordinateTranslation(){
-		int [][] emptySet = new int[512][512];
 		MandelbrotSet yCoordTest = new MandelbrotSet();
 	}
 	
 	@Test
 	public void testMandelbrotETNeverExceedsED(){
-		int [][] emptySet = new int[512][512];
 		MandelbrotSet neverExceedsED = new MandelbrotSet();
+		assertEquals("Product is 255.", 255, neverExceedsED.mandelbrotSet(0.3207031250000001, -0.07109374999999386), 0.0001);
 	}
 	@Test
 	public void testMandelbrotETExceedsED(){
-		int [][] emptySet = new int[512][512];
 		MandelbrotSet etExceedsED = new MandelbrotSet();
+		assertEquals("Product is 255.", 255,etExceedsED.mandelbrotSet(0.5946289062500001, 1.2949218750000122), 0.0001);
 	}
 	@Test
 	public void testMandelbrotReturn(){
-		int[][] emptySet = new int [512][512];
 		MandelbrotSet returns2DArray = new MandelbrotSet();
 	}
+	
 	//Julia Set Tests
 	
 	@Test
 	public void testJuliaXCoordinateTranslation(){	//name of class: JuliaSet
-		int [][] emptySet = new int[512][512];
 		JuliaSet xCoordTest = new JuliaSet();
 		
 	}
 	@Test
 	public void testJuliaYCoordinateTranslation(){
-		int [][] emptySet = new int[512][512];
 		JuliaSet yCoordTest = new JuliaSet();
 		
 	}
 	@Test
 	public void testJuliaETNeverExceedsED(){
-		int [][] emptySet = new int[512][512];
 		JuliaSet neverExceedsED = new JuliaSet();
 	}
 	@Test
 	public void testJuliaETExceedsED(){
-		int [][] emptySet = new int[512][512];
 		JuliaSet etExceedsED = new JuliaSet();
 	}
 	@Test
 	public void testJuliaReturn(){
-		int[][] emptySet = new int [512][512];
 		JuliaSet returns2DArray = new JuliaSet();
 	}
+	
 	//Burning Ship Set Tests
 	
 	@Test
 	public void testBurningShipXCoordinateTranslation(){	//name of class: burningshipset
-		int [][] emptySet = new int[512][512];
 		burningshipset xCoordTest = new burningshipset();
 		
 	}
 	@Test
 	public void testBurningShipYCoordinateTranslation(){
-		int [][] emptySet = new int[512][512];
 		burningshipset yCoordTest = new burningshipset();
 		
 	}
 	@Test
 	public void testBurningShipETNeverExceedsED(){
-		int [][] emptySet = new int[512][512];
 		burningshipset neverExceedsED = new burningshipset();
 	}
 	@Test
 	public void testBurningShipETIsNot0or1(){	//unique test for Burning Ship Set
-		int [][] emptySet = new int[512][512];
 		burningshipset etIsNot0or1 = new burningshipset();
 	}
 	@Test
 	public void testBurningShipReturn(){
-		int[][] emptySet = new int [512][512];
 		burningshipset returns2DArray = new burningshipset();
 	}
 }
