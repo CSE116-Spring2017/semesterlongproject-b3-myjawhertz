e589c3fc547cdcdb44d583ce7bd2d0724b1a3751
user <user@128.205.208.203>
2017-02-19 18:40:47 -0500

    burningshipset

--- src/edu/buffalo/cse116/burningshipset.java
+++ src/edu/buffalo/cse116/burningshipset.java
@@ -1,6 +1,8 @@
 package edu.buffalo.cse116;
 
 public class burningshipset {
+	private double xcor;
+	private double ycor;
 	public int[][] Burningshipset(double xCalc, double yCalc){
 		int[][] burningshipset = new int[512][512];
 		
@@ -12,8 +14,15 @@ public class burningshipset {
 		while (dist <= 4 && passes < 255){
 			double x = xCalc;
 			double y = yCalc;
+			
+			xcor = x;
+			ycor = y;
+			
+			
 			xCalc = xCalc + (3.5/512);
-			yCalc = yCalc + (2.0/512);
+			yCalc = yCalc + (0.105/512);
+			
+			
 					
 			double xprime = (xCalc * xCalc) - (yCalc * yCalc) + x;
 			double yprime = Math.abs(2 * xCalc * yCalc) + y;
@@ -23,15 +32,18 @@ public class burningshipset {
 			dist = Math.sqrt((xCalc * xCalc) + (yCalc * yCalc)); 
 		}
 		//Set the current point's escape-time equal to passes 
-	
-		
-		
-		
-		
-		
 		return burningshipset;
 		
 	}
-	
+	public void translation(double X,double Y){
+		X = xcor;
+		Y = ycor;
+		
+	}
 }
+
+
+
+
+	
 // none of the pixels in the burningshipset have an escape time of 0 or 1
