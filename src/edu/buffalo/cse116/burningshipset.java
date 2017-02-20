654714f2c566749ab75f33dbb19d38f7e79aa26f
user <user@128.205.208.203>
2017-02-19 19:29:24 -0500

    burningshipsetdd

--- src/edu/buffalo/cse116/burningshipset.java
+++ src/edu/buffalo/cse116/burningshipset.java
@@ -9,27 +9,29 @@ public class burningshipset {
 		int passes = 0;
 		
 		while (dist <= 4 && passes < 255){
-			double x = xCalc;
-			double y = yCalc;
+			double x = xCalc;//current point of x
+			double y = yCalc;//current point of y
 						
 			xCalc = xCalc + (3.5/512);
 			yCalc = yCalc + (0.105/512);
 				
-			double xprime = (xCalc * xCalc) - (yCalc * yCalc) + x;
-			double yprime = Math.abs(2 * xCalc * yCalc) + y;
-			xCalc = xprime;
-			yCalc = yprime;
+			double xprime = (xCalc * xCalc) - (yCalc * yCalc) + x;//calculating x-coordinate
+			double yprime = Math.abs(2 * xCalc * yCalc) + y;//calculating y-coordinate
+			xCalc = xprime;//updated value of x into xCalc
+			yCalc = yprime;//updated value of y into yCalc
 			passes++;
 			dist = Math.sqrt((xCalc * xCalc) + (yCalc * yCalc)); 
 		}
 		return burningshipset;
 	}
+	//method testing translation of x-cord
 	public double xcor(int rows){
-		double xcord = -1.8;  
+		double xcord = -1.8;
 		double sum	= 3.5 * (rows/512);
 		xcord = xcord + sum;
 		return xcord;
 	}
+	//method testing translation of y-cord
 	public double ycor(int cols){
 		double ycord = -0.08;
 		double sum = 0.105 * (cols/512);
