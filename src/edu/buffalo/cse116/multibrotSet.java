1628c1722026659707d8391d68c62670a6b7f2bf
alec1 <alec1@10.84.123.214>
2017-02-24 15:17:21 -0500

    added final JavaDoc comments to the class

--- src/edu/buffalo/cse116/multibrotSet.java
+++ src/edu/buffalo/cse116/multibrotSet.java
@@ -1,19 +1,37 @@
 package edu.buffalo.cse116;
 
 public class multibrotSet {
+	/**
+	 * This class provides the translations and escape-time algorithms for the
+	 * MultiBrot Set
+	 * @author Alec Otminski
+	 */
 	
 	private int[][] array = new int[512][512];
-	
+	/**
+	 * Translates x-coordinates from 0-512 ----> -1 to 1
+	 * @param x original x-coordinate in 512 x 512 array
+	 * @return new xCalc value for this set
+	 */
 	public double XCoordTranslation(int x){
 		double xCalc = -1 + ((2 * x) / 512);
 		return xCalc;
 	}
-	
+	/**
+	 * Translates y-coordinates from 0-512 ----> -1.3 to 1.3 
+	 * @param y original y-coordinate in 512 x 512 array
+	 * @return new yCalc value for this set
+	 */
 	public double YCoordTranslation(int y){
 		double yCalc = -1.3 + ((2.6 * y) / 512);
 		return yCalc;
 	}
-	
+	/**
+	 * This method does the Escape-Time Calculations for the MultiBrot Set
+	 * @param xCalc - a translated x-Coordinate in the MultiBrot Set
+	 * @param yCalc - a translated y-Coordinate in the MultiBrot Set
+	 * @return the Escape Time for the set == number of passes done
+	 */
 	private double dist;
 	private int passes = 0;
 	public int mbSetETCalculation(double xCalc, double yCalc) {
