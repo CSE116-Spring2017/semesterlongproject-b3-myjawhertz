2340b7dacfd66821551767895cbf166a40e34710
alec1 <alec1@10.84.72.131>
2017-03-10 15:28:37 -0500

    Finally fixed the issue with the multibrot set
    Turns out forgetting to put 2.0 instead of just a 2 for calculating
    doubles will lead to some dumb calculations.

--- src/edu/buffalo/cse116/multibrotSet.java
+++ src/edu/buffalo/cse116/multibrotSet.java
@@ -13,7 +13,9 @@ public class multibrotSet {
 	 * @return new xCalc value for this set
 	 */
 	public double XCoordTranslation(int x){
-		double xCalc = -1 + (2 * x / 512);
+		System.out.println("Original x-value is:" + x);
+		double xCalc = -1 + (2.0 * x / 512);
+		System.out.println("New x-value for " + x + " is: " + xCalc);
 		return xCalc;
 	}
 	/**
@@ -22,7 +24,9 @@ public class multibrotSet {
 	 * @return new yCalc value for this set
 	 */
 	public double YCoordTranslation(int y){
+		System.out.println("Original y-value is: " + y);
 		double yCalc = -1.3 + (2.6 * y / 512);
+		System.out.println("New y-value for " + y + " is: " + yCalc);
 		return yCalc;
 	}
 	
@@ -35,7 +39,7 @@ public class multibrotSet {
 	public int mbSetETCalculation(double xCalc, double yCalc) {
 		double x = xCalc;
 		double y = yCalc;
-		double dist = 0;
+		double dist = Math.sqrt(Math.pow(xCalc, 2) + Math.pow(yCalc, 2));
 		int passes = 0;
 		while (dist <= 2 && passes < 255) {
 			double tempX = xCalc;	
@@ -51,10 +55,10 @@ public class multibrotSet {
 	public int multibrotsetPass10(double xCalc, double yCalc) {
 		double x = xCalc;
 		double y = yCalc;
-		double dist = 0;
+		double dist = Math.sqrt(Math.pow(xCalc, 2) + Math.pow(yCalc, 2));
 		int passes = 0;
 		while (dist <= 3 && passes < 255) {
-			double tempX = xCalc;	
+			double tempX = xCalc;
 			double tempY = yCalc;
 			xCalc = (tempX * tempX * tempX) - (3 * tempX * (tempY * tempY)) + x;
 			yCalc = (3 * (tempX * tempX) * tempY) - (tempY * tempY * tempY) + y;
@@ -63,16 +67,18 @@ public class multibrotSet {
 		}
 		return passes;
 	}
-	
+	/**
+	 * This method gives the escape-times of each pixel in a 2D array form.
+	 * @return A 2D Array of int containing the escape time of each pixel in the 512 x 512 array. 
+	 */
 	public int[][] return2DArray(){
 		int[][] array = new int[512][512];
 		for (int r = 0; r < 512; r++){		//for each row in the array
 			for (int c = 0; c < 512; c++) {		//for each column in the array
 				array[r][c] = mbSetETCalculation(XCoordTranslation(r),YCoordTranslation(c));
-//				System.out.println(array[r][c]);	<---this line is for debugging purposes
+//				System.out.println(array[r][c]);	//this line is for debugging purposes
 			}
 		}
 		return array;
 	}
-	
 }
