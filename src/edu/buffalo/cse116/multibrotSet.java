172fd8abdd7db8235c9461f9cdae8783feabb0cb
sfung3 <sfung3@10.84.25.76>
2017-03-09 21:17:01 -0500

    added a method to pass a JUnit test

--- src/edu/buffalo/cse116/multibrotSet.java
+++ src/edu/buffalo/cse116/multibrotSet.java
@@ -48,12 +48,28 @@ public class multibrotSet {
 		return passes;
 	}
 	
+	public int multibrotsetPass10(double xCalc, double yCalc) {
+		double x = xCalc;
+		double y = yCalc;
+		double dist = 0;
+		int passes = 0;
+		while (dist <= 3 && passes < 255) {
+			double tempX = xCalc;	
+			double tempY = yCalc;
+			xCalc = (tempX * tempX * tempX) - (3 * tempX * (tempY * tempY)) + x;
+			yCalc = (3 * (tempX * tempX) * tempY) - (tempY * tempY * tempY) + y;
+			dist = Math.sqrt(Math.pow(xCalc, 2) + Math.pow(yCalc, 2));
+			passes++;
+		}
+		return passes;
+	}
+	
 	public int[][] return2DArray(){
 		int[][] array = new int[512][512];
 		for (int r = 0; r < 512; r++){		//for each row in the array
 			for (int c = 0; c < 512; c++) {		//for each column in the array
 				array[r][c] = mbSetETCalculation(XCoordTranslation(r),YCoordTranslation(c));
-				System.out.println(array[r][c]);
+//				System.out.println(array[r][c]);
 			}
 		}
 		return array;
