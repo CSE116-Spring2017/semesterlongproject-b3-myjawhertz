df5a06d894cfa7668482dc3ef94a4cb49e2a622b
sfung3 <sfung3@10.84.25.76>
2017-03-09 19:57:58 -0500

    Fixed return 2d array with passes + added a return empty array method

--- src/edu/buffalo/cse116/burningshipset.java
+++ src/edu/buffalo/cse116/burningshipset.java
@@ -2,71 +2,70 @@ package edu.buffalo.cse116;
 
 //class for burningshipset
 public class burningshipset {
-	
-	//@para value to be calculated for current x point
-	//@para value to be calculated for currnet y point
-	private int passes = 0;
-	public int Burningshipset(double xCalc, double yCalc){		
-		double dist = Math.sqrt((xCalc * xCalc) + (yCalc * yCalc));;
-		double x = xCalc;//current point of x
-		double y = yCalc;//current point of y
-		
-		while (dist <= 2 && passes < 255){
-			double xxx = xCalc; //x value before update
-			double yyy = yCalc;	//y value before update
-			xCalc = (xxx * xxx) - (yyy * yyy) + x;//calculating x-coordinate and updating
-			yCalc = Math.abs(2 * xxx * yyy) + y;//calculating y-coordinate and updating
-			dist = Math.sqrt((xCalc * xCalc) + (yCalc * yCalc)); 
-			passes += 1;
-			
+	public double getXCord(int xRow) {
+		double xCord = -1.8 + (.1 * xRow / 512);
+		return xCord;
+	}
+
+	/**
+	 * translate from 0-512 row to yCord
+	 * @param yCol - column from 0 - 512
+	 * @return y coordinate
+	 **/
+	public double getYCord(int yCol) {
+		double yCord = -0.08 + (.105 * yCol / 512);
+		return yCord;
+	}
+
+//	private double dist;
+//	private int passes = 0;
+
+	/**
+	 * calculates passes
+	 * 
+	 * @param xCalc - X-coordinate range from -2.15 to 0.6
+	 * @param yCalc - Y-coordinate range from -1.3 to 1.3
+	 * @return number of passes for the inserted xCalc and yCalc
+	 **/
+	public int burningShip(double xCalc, double yCalc) {
+		double x = xCalc;
+		double y = yCalc;
+		double dist = 0;
+		int passes = 0;
+		while (dist <= 2 && passes < 255) {
+			double tempX = xCalc;	
+			double tempY = yCalc;
+
+			xCalc = (tempX * tempX) - (tempY * tempY) + x;
+			yCalc = Math.abs(2*tempX*tempY) + y;
+
+			dist = Math.sqrt(Math.pow(xCalc, 2) + Math.pow(yCalc, 2));
+
+			passes++;
 		}
-		return passes; 
-	} 
-	//method calculating translation of x-coord
-	//@para x value before calculation
-	//@return calculated xcoord value
-	public double xcor(int rows){
-		double xcord = -1.8 + 3.5 * (rows/512);
-		return xcord;
-	}
-	//method calculating translation of y-coord
-	//@para y value before calculation
-	//@return calculated ycoord vale
-	public double ycor(int cols){
-		double ycord = -0.08 +  0.105 * (cols/512);
-		return ycord;
+
+		return passes;
 	}
 	
-	//@return return to array with 512 rows and 512 cols
-	public int[][] returnarray(){
-		int[][] returnEmptyArray = new int[512][512];
-		return returnEmptyArray;
-	}
-	
-	public int[][] returnArrayWithPasses(){
+	public int[][] return2DArray(){
 		int[][] array = new int[512][512];
-		for(int a = 0; a < 512; a++){
-			for(int b = 0; b < 512; b++){
-				array[a][b] = Burningshipset(xcor(a),ycor(b));
+		return array;
+	}
+
+
+	/**
+	 * creates a 2D array with passes for each point
+	 * @return 2D array with passes for each point
+	 */
+	public int[][] returnArrayWithPasses() {
+		int[][] array = new int[512][512];
+		for(int x = 0; x < 512; x++){
+			for(int y = 0; y < 512; y++){
+				array[x][y] = burningShip(getXCord(x), getYCord(y));	
+				System.out.println(array[x][y]);
 			}
-		}  
+		}
 		return array;
 	}
 	
-	//method to be used for test whether the escape-time become 0 or 1
-	//@return return to array type int with 512 rows and 512 cols
-	public boolean test() {
-		int x = 0;
-		int y = 0;
-		for (x = 0; x < 512; x++) {
-			for (y = 0; y < 512; y++) {
-				Burningshipset(xcor(x),ycor(y));
-				if (Burningshipset(xcor(x),ycor(y)) > 2) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
 }
-
